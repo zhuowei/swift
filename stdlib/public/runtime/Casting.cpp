df6f255c9b4fedb89fb61a2ce463efd6bdbc8295
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2015-12-06 00:17:44 -0500

    android: stub out some missing methods on Android, disable split dwarf by default

--- stdlib/public/runtime/Casting.cpp
+++ stdlib/public/runtime/Casting.cpp
@@ -2398,6 +2398,10 @@ static void _addImageProtocolConformances(const mach_header *mh,
   _addImageProtocolConformancesBlock(conformances, conformancesSize);
 }
 #elif defined(__ELF__)
+#ifdef __ANDROID__
+#define RTLD_NOLOAD 0
+#endif
+
 static int _addImageProtocolConformances(struct dl_phdr_info *info,
                                           size_t size, void * /*data*/) {
   void *handle;
@@ -2438,7 +2442,10 @@ static void installCallbacksToInspectDylib() {
     // loaded ones.
     // FIXME: Find a way to have this continue to happen after.
     // rdar://problem/19045112
+    #ifndef __ANDROID__
+    // FIXME: Android: doesn't have dl_iterate_phdr before API 21
     dl_iterate_phdr(_addImageProtocolConformances, nullptr);
+    #endif
   #else
   # error No known mechanism to inspect dynamic libraries on this platform.
   #endif

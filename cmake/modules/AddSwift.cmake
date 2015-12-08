4509f6fc8e28330a4f944d675d8e463d462e6b4a
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2015-12-07 22:39:10 -0500

    android: add log2 implementation from Android Support and mulodi4 from compiler_rt, hardcode path to libicu for now

--- cmake/modules/AddSwift.cmake
+++ cmake/modules/AddSwift.cmake
@@ -199,8 +199,9 @@ function(_add_variant_link_flags
   elseif("${sdk}" STREQUAL "ANDROID")
     list(APPEND result
         "-ldl"
-        "-L${SWIFT_ANDROID_NDK_PATH}/toolchains/arm-linux-androideabi-4.8/prebuilt/linux-x86_64/lib/gcc/arm-linux-androideabi/4.8/thumb"
-        "${SWIFT_ANDROID_NDK_PATH}/sources/cxx-stl/llvm-libc++/libs/armeabi-v7a/thumb/libc++_shared.so")
+        "-L${SWIFT_ANDROID_NDK_PATH}/toolchains/arm-linux-androideabi-4.8/prebuilt/linux-x86_64/lib/gcc/arm-linux-androideabi/4.8"
+        "${SWIFT_ANDROID_NDK_PATH}/sources/cxx-stl/llvm-libc++/libs/armeabi-v7a/libc++_shared.so"
+        "-L/home/zhuowei/libiconv-libicu-android/armeabi-v7a")
   else()
     list(APPEND result "-lobjc")
   endif()

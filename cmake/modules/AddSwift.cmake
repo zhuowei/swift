99f1995285251b1645b45b7c121c06e0aa7b2b1a
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2015-12-08 02:11:49 -0500

    android: start working on bionic wrapper module; pass Bsymbolic to stop linker from complaining

--- cmake/modules/AddSwift.cmake
+++ cmake/modules/AddSwift.cmake
@@ -198,6 +198,7 @@ function(_add_variant_link_flags
     # No extra libraries required.
   elseif("${sdk}" STREQUAL "ANDROID")
     list(APPEND result
+        "-Wl,-Bsymbolic"
         "-ldl"
         "-L${SWIFT_ANDROID_NDK_PATH}/toolchains/arm-linux-androideabi-4.8/prebuilt/linux-x86_64/lib/gcc/arm-linux-androideabi/4.8"
         "${SWIFT_ANDROID_NDK_PATH}/sources/cxx-stl/llvm-libc++/libs/armeabi-v7a/libc++_shared.so"
